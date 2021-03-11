b935dd19868e5de9725d0ed363dc564cd2c50192
AntonKurtsev <kucrev@gmail.com>
2021-03-12 00:17:30 +0300

    add: index page; fixed: calc input, history text

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    // History text
+    const historyBtn = document.querySelector('.history__btn');
+    const historyTextBlock = document.querySelector('.history__text-block');
+    const historyText = document.querySelector('.history__text');
+
+    historyBtn.addEventListener('click', () => {
+        historyBtn.classList.toggle('-maximized');
+        historyTextBlock.classList.toggle('-maximized');
+
+        let historyTextBlockHeight = historyTextBlock.getBoundingClientRect().height;
+        let historyTextHeight = historyText.getBoundingClientRect().height;
+
+        if (historyTextHeight > historyTextBlockHeight) {
+            historyTextBlock.style.height = historyTextHeight + 'px';
+        } else {
+            historyTextBlock.style.height = 384 + 'px';
+        };
+    });
+
+    // Calculator
     const calcInput = document.querySelector('.calc__enter-input');
     const calcBtn = document.querySelector('.calc__btn');
     const calcResult = document.querySelector('.calc__result-value');
