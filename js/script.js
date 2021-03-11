cfcdd957636a0fa0adeb3acc2721c04729086b45
AntonKurtsev <kucrev@gmail.com>
2021-03-12 01:05:47 +0300

    add: smooth scroll to section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    // Плавный скролл к секциям
+    const smoothLinks = document.querySelectorAll('a[href^="#"]');
+    console.log(smoothLinks.length);
+    for (let smoothLink of smoothLinks) {
+        smoothLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            smoothLinks.forEach(element => {
+                element.parentElement.classList.remove('-active')
+            });
+            smoothLink.parentElement.classList.add('-active');
+            const id = smoothLink.getAttribute('href');
+
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    };
+
     // History text
     const historyBtn = document.querySelector('.history__btn');
     const historyTextBlock = document.querySelector('.history__text-block');
